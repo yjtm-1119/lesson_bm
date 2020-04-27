59af19b09ae06eb3b5a692b3498e6d4eee63bf54
2104984884 <2104984884@qq.com>
2020-04-27 20:15:29 +0800

    g

---
+++ js-fullsta/append/read.md
@@ -0,0 +1,10 @@
+- DOM document 文档就是网页  js内存里  文档对象模型
+    js ->html 转化
+    DOM JS 数据结构tree  appendChild  组成一棵树
+
+    ul>li>text>node
+    document
+
+
+
+    哪怕是文字 都是节点 都是对象
---
+++ leetcode/1552/1.js
@@ -0,0 +1,11 @@
+var findNumberIn2DArray = function (matrix, target) {
+    
+};
+const matrix = [
+    [1, 4, 7, 11, 15],
+    [2, 5, 8, 12, 19],
+    [3, 6, 9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30]
+]
+findNumberIn2DArray(matrix,5)
